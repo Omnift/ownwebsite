8e7a54c20fc977bf7a2717828bbe90d05cbca2fc
Omnift <44648174+Omnift@users.noreply.github.com>
2018-10-31 22:35:15 +0530

    Add files via upload

---
+++ ownwebsite/js/main.js
@@ -0,0 +1,5 @@
+// let div1 = document.querySelector(".div1");
+// let div2 = document.querySelector(".div2");
+// let height = 300;
+// div1.setAttribute('style','height:'+window.outerHeight+'px');
+// div2.setAttribute('style','height:'+window.outerHeight+'px');
